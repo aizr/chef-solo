ce9a859085dca355d5d61b8caed744d21be42dae
aizr <aizr1212@gmail.com>
2013-07-26 11:42:12 +0800

    modify ruby version comment

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 sudo apt-get -y update
 sudo apt-get --no-install-recommends -y install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev libgdbm-dev ncurses-dev automake libtool bison subversion pkg-config libffi-dev vim
 
-# Download and compile Ruby 2.0.0-p0
+# Download and compile Ruby 1.9.3-p327
 cd /tmp
 wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p327.tar.gz
 tar -xvzf ruby-1.9.3-p327.tar.gz

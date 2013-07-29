41b1ca6ab0004be46852961b8d15e94b49d661a1
aizr <aizr1212@gmail.com>
2013-07-29 17:15:09 +0800

    add user for main cookbook

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -6,3 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+user "deployer" do
+  password "$1$mfiBUi8F$0HdIS6iIttKW8pgkafuaB0"
+  home "/home/deployer"
+  shell "/bin/bash"
+  supports :manage_home => true
+end
